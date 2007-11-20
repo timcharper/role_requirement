e762853b8d8080234b4ed05e591e32303f06b24f
timcharper <timcharper@0128ec72-9f35-0410-83f6-31648bef6b25>
2007-11-20 07:05:31 +0000

    increased test coverage, added tests for aliases
    git-svn-id: http://rolerequirement.googlecode.com/svn/trunk@102 0128ec72-9f35-0410-83f6-31648bef6b25

--- test/functional/test_role_controller.rb
+++ test/functional/test_role_controller.rb
@@ -20,7 +20,7 @@ class TestRoleController < Test::Unit::TestCase
   
   def test__required_role_for_all__should_require
     @controller.class.require_role "admin"
-    @controller.current_user = User.new(:roles => "")
+    set_roles ""
     
     assert(!@controller.check_roles)
 
@@ -29,8 +29,8 @@ class TestRoleController < Test::Unit::TestCase
   end
   
   def test_required_role_for_some_actions_only__should_require
-    @controller.class.require_role "admin", :only => [:update, :destroy, :delete]
-    @controller.current_user = User.new(:roles => "")
+    require_role "admin", :only => [:update, :destroy, :delete]
+    set_roles ""
     
     @controller.params = {:action => "update" }
     assert(!@controller.check_roles, "shouldn't pass")
@@ -39,9 +39,25 @@ class TestRoleController < Test::Unit::TestCase
     assert(@controller.check_roles, "should pass")
   end
   
+  def test__for_alias
+    require_role "admin", :for => [:update, :destroy, :delete]
+    
+    set_roles ""
+    assert_fails_with :action => "update"
+    assert_passes_with :action => "index"
+  end
+  
+  def test__for_all_except_alias
+    require_role "admin", :for_all_except => [:index]
+    
+    set_roles ""
+    assert_fails_with :action => "update"
+    assert_passes_with :action => "index"
+  end
+  
   def test_required_role_for_excluded_actions_only__should_require
-    @controller.class.require_role "admin", :except => :index
-    @controller.current_user = User.new(:roles => "")
+    require_role "admin", :except => :index
+    set_roles ""
     
     @controller.params = {:action => "update" }
     assert(!@controller.check_roles, "shouldn't pass")
@@ -69,7 +85,7 @@ class TestRoleController < Test::Unit::TestCase
   
   def test_required_role_for_if_proc__should_require
     @controller.class.require_role "admin", :only => :list, :if => Proc.new {|params| params[:status] == "completed" }
-    @controller.current_user = User.new(:roles => "")
+    set_roles ""
     
     @controller.params = {:action => "list", :status => "pending" }
     assert(@controller.check_roles, "should pass")
@@ -89,7 +105,7 @@ class TestRoleController < Test::Unit::TestCase
     assert(@controller2.check_roles, "should pass")
 
     @controller2.current_user = User.new(:roles => "")
-    @controller.current_user = User.new(:roles => "")
+    set_roles ""
     assert(!@controller.check_roles, "shouldn't pass")
     assert(!@controller2.check_roles, "shouldn't pass")
   end
@@ -98,7 +114,7 @@ class TestRoleController < Test::Unit::TestCase
     MyController.require_role "admin", :only => :destroy
     OtherController.require_role "admin", :only => :destroy
     
-    @controller.current_user = User.new(:roles => "")
+    set_roles ""
     
     assert ! @controller.send(:url_options_authenticate?, { :controller => :my, :action => "destroy" } ), "shouldn't pass"
     assert ! @controller.send(:url_options_authenticate?, { :controller => :other, :action => "destroy" } ), "shouldn't pass"
@@ -115,7 +131,7 @@ class TestRoleController < Test::Unit::TestCase
   
   def test__url_options_authenticate__nil_value__should_treat_as_index
     MyController.require_role "admin", :only => :index
-    @controller.current_user = User.new(:roles => "")
+    set_roles ""
 
     assert ! @controller.send(:url_options_authenticate?, { :controller => :my } ), "shouldn't pass"
     assert @controller.send(:url_options_authenticate?, { :controller => :my, :action => "destroy" } ), "should pass"
@@ -123,10 +139,37 @@ class TestRoleController < Test::Unit::TestCase
   
   def test__url_options_authenticate__no_controller__assumes_self
     MyController.require_role "admin", :only => :index
-    @controller.current_user = User.new(:roles => "")
+    set_roles ""
 
     assert ! @controller.send(:url_options_authenticate?, { :action => "index" } ), "shouldn't pass"
     assert @controller.send(:url_options_authenticate?, { :action => "boogy" } ), "shouldn't pass"
   
   end
+
+protected
+  def require_role(*params)
+    @controller.class.require_role(*params)
+  end
+  
+  def reset_role_requirements!
+    @controller.class.reset_role_requirements!
+  end
+  
+  def set_roles(roles = "")
+    @controller.current_user = User.new(:roles => roles)
+  end
+  
+  def passes_with_params?(params = {})
+    @controller.params = params
+    @controller.check_roles
+  end
+  
+  def assert_passes_with(params = {})
+    assert passes_with_params?(params), "should pass"
+  end
+  
+  def assert_fails_with(params = {})
+    assert ! passes_with_params?(params), "should pass"
+  end
+  
 end
